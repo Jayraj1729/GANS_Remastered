c8bd65c02fea337fbe59fc5588f7bb8303bc5177
Jayraj Karelia <76041245+Jayraj1729@users.noreply.github.com>
2023-04-18 13:36:58 +0530

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,14 @@
+import openpyxl
+import os
+
+
+
+
+wb = openpyxl.load_workbook('data.xlsx')
+sheet = wb.active
+list(sheet.columns)[0]
+
+for cellObj in list(sheet.columns)[0]:
+    os.system("grype %s >result/%s.txt"% (cellObj.value, cellObj.value.replace("/","_")))
+    os.system("docker rmi %s"%cellObj.value.replace("/", "_"))
+   #print(cellObj.value)
